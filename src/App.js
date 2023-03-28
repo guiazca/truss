1f0e0dde3985f3ad0ad0536dc3ab79229a0b003c
Guilherme Carvalho <guiazca@gmail.com>
2023-03-28 07:35:13 +0100

    change vendor name

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
   render() {
     return (
       <Container>
-        <text>Vendedor: Márcia e Angélica</text>
+        <text>Vendedor: Paula Paulino</text>
         <form className="Produto">
           <Row>
             <Col sm="8">
