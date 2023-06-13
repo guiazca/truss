0279ef49ab090bbaecbb0b86de5ec7db4514f230
Guilherme Carvalho <guiazca@gmail.com>
2023-06-13 08:43:49 +0100

    products table improve

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
   render() {
     return (
       <Container>
-        <text>Vendedor: Márcia e Angélica</text>
+        <text>Vendedor: Márcia Brasil</text>
         <form className="Produto">
           <Row>
             <Col sm="8">
