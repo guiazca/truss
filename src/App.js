27035262e2cf523d0a9fcdf8634935a351c81855
Guilherme Carvalho <guiazca@gmail.com>
2023-06-13 09:10:39 +0100

    name update

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
   render() {
     return (
       <Container>
-        <text>Vendedor: Márcia Brasil</text>
+        <text>Vendedor: Cesar</text>
         <form className="Produto">
           <Row>
             <Col sm="8">
